1e5d896a52deacea62b6fcec30375f73e564ddf5
ebtesam25 <ebtesam@iut-dhaka.edu>
2021-05-16 01:25:33 +0300

    landing screen complete

--- mobile-app/app/src/screens/welcome.js
+++ mobile-app/app/src/screens/welcome.js
@@ -4,6 +4,8 @@ import { useNavigation } from '@react-navigation/native';
 import Svg, { Line } from 'react-native-svg';
 import { Icon } from 'react-native-elements'
 import { useFonts } from 'expo-font';
+import { LinearGradient } from 'expo-linear-gradient';
+
 
 export default function Welcome() {
     const navigation = useNavigation();
@@ -19,13 +21,33 @@ export default function Welcome() {
     return (
         <View style={styles.container}>
             <ImageBackground source={require('../assets/bg.png')} style={{height:'100%', width:'100%'}} imageStyle={{resizeMode:'cover', alignSelf:'flex-end'}}>
-            <View style={{marginHorizontal:'5%', marginTop:'10%'}}>
-                <View style={{marginTop:'50%'}}></View>
+            <View style={{marginHorizontal:'7.5%', marginTop:'10%'}}>
+                <View style={{marginTop:'60%'}}></View>
                 <Image source={require('../assets/logo.png')} style={styles.logo}></Image>
+                <Text style={{fontFamily:'H', fontSize:40, color:"#FFF"}}><Text style={{color:"#FBCACA"}}>Redi</Text>Safe</Text>
+                <Text style={{fontFamily:'B', fontSize:18, width:'70%', color:"#FFF"}}>Health monitoring, COVID data, yada yada lorem ipsum bruh</Text>
+                <View style={{marginTop:'15%'}}></View>
                 <TouchableOpacity onPress={()=>navigation.navigate('Signup')}>
-                    <View style={styles.btn}>
-                    <Text style={styles.title}>Get Started</Text>
+                <LinearGradient
+                    // Button Linear Gradient
+                    colors={['#FFFFFF', '#F4ACAC']}
+                    start={[1,-0.3]}
+                    end={[1,1]}
+                    style={styles.btn}>
+                    <View style={{flexDirection:'row', justifyContent:'space-between'}}>
+                    <Text style={styles.btnlabel}>Login</Text>
+                    <Icon name="rightcircle" type="ant-design" color="#F04D4E" style={{textAlign:'right'}}></Icon>
                     </View>
+                </LinearGradient>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={()=>navigation.navigate('Signup')}>
+                <View style={styles.btn2}>
+                    <View style={{flexDirection:'row', justifyContent:'space-between'}}>
+                    <Text style={styles.btnlabel2}>Sign up</Text>
+                    <Icon name="rightcircle" type="ant-design" color="#FFF" style={{textAlign:'right'}}></Icon>
+                    </View>
+                </View>
                 </TouchableOpacity>
 
                 </View>
@@ -53,10 +75,11 @@ const styles = StyleSheet.create({
         alignSelf:'flex-start',
     },
     title: {
-        fontFamily:'B',
+        fontFamily:'H',
         color:"#FFF",
         fontSize:20,
-        textAlign:'center'
+        textAlign:'center',
+        color:'#F04D4E'
     },
     subtitle: {
         fontFamily:'B',
@@ -68,12 +91,39 @@ const styles = StyleSheet.create({
         alignSelf:'center'
     },
     btn: {
-        backgroundColor:`linear-gradient(90deg, rgba(239,246,251,1) 0%, rgba(244,172,172,1) 50%);`,
-        paddingVertical:'2.5%',
+        backgroundColor:"#F4ACAC",
+        paddingVertical:'5%',
         width:'90%',
         alignSelf:'center',
-        borderRadius:20,
-        marginTop:'10%'
+        borderRadius:15,
+        marginTop:'10%',
+        paddingHorizontal:'10%',
+
+    },
+    btnlabel: {
+        fontFamily:'H',
+        fontSize:20,
+        textAlign:'left',
+        color:'#F04D4E'
+
+    },
+    btn2: {
+        backgroundColor:`rgba(255, 255, 255, 0.1)`,
+        paddingVertical:'5%',
+        width:'90%',
+        alignSelf:'center',
+        borderRadius:15,
+        marginTop:'5%',
+        paddingHorizontal:'10%',
+        borderColor:`rgba(252, 219, 220, 0.5)`,
+        borderWidth:2
+
+    },
+    btnlabel2: {
+        fontFamily:'H',
+        fontSize:20,
+        textAlign:'left',
+        color:'#FFF'
 
     }
 
